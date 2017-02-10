9e5fabb48fd6c15489b7966c7aba8f2e3b6075a3
Yashraj Jhala <jhalayashraj@gmail.com>
2017-02-10 14:58:56 +0000

    Revert "CMParts: Long-press power while display is off for torch"
    This reverts commit f1ccf803280e9d0a048b906eff4ddb365c416363.

--- src/org/cyanogenmod/cmparts/input/ButtonSettings.java
+++ src/org/cyanogenmod/cmparts/input/ButtonSettings.java
@@ -42,7 +42,6 @@ import org.cyanogenmod.cmparts.R;
 import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
 import org.cyanogenmod.cmparts.utils.DeviceUtils;
 import org.cyanogenmod.cmparts.utils.TelephonyUtils;
-import org.cyanogenmod.internal.util.QSUtils;
 import org.cyanogenmod.internal.util.ScreenType;
 
 import java.util.List;
@@ -74,8 +73,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_HOME_ANSWER_CALL = "home_answer_call";
     private static final String KEY_VOLUME_MUSIC_CONTROLS = "volbtn_music_controls";
     private static final String KEY_VOLUME_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
-    private static final String KEY_TORCH_LONG_PRESS_POWER_GESTURE =
-            "torch_long_press_power_gesture";
 
     private static final String CATEGORY_POWER = "power_key";
     private static final String CATEGORY_HOME = "home_key";
@@ -147,7 +144,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mNavigationRecentsLongPressAction;
     private SwitchPreference mPowerEndCall;
     private SwitchPreference mHomeAnswerCall;
-    private SwitchPreference mTorchLongPressPowerGesture;
 
     private PreferenceCategory mNavigationPreferencesCat;
 
@@ -205,10 +201,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
         // Power button ends calls.
         mPowerEndCall = (SwitchPreference) findPreference(KEY_POWER_END_CALL);
-
-        // Long press power while display is off to activate torchlight
-        mTorchLongPressPowerGesture =
-                (SwitchPreference) findPreference(KEY_TORCH_LONG_PRESS_POWER_GESTURE);
 
         // Home button answers calls.
         mHomeAnswerCall = (SwitchPreference) findPreference(KEY_HOME_ANSWER_CALL);
@@ -281,9 +273,6 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             if (!TelephonyUtils.isVoiceCapable(getActivity())) {
                 powerCategory.removePreference(mPowerEndCall);
                 mPowerEndCall = null;
-            }
-            if (!QSUtils.deviceSupportsFlashLight(getActivity())) {
-                powerCategory.removePreference(mTorchLongPressPowerGesture);
             }
         } else {
             prefScreen.removePreference(powerCategory);
